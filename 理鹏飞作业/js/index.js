e4fe463aafbe0574930783fbb4e4c28d6cfd9a10
lipenfei <1515066828@qq.com>
2020-04-24 20:07:38 +0800

    完成项目

--- 理鹏飞作业/js/index.js
+++ 理鹏飞作业/js/index.js
@@ -91,43 +91,191 @@ $(function () {
         }
     });
 });
+
+Object.keys($("#diyheadBottomTextoneleft li")).forEach((index, item) => {
+    if (index * 1 || index * 1 == 0) {
+        $("#diyheadBottomTextoneleft li")[index].liindex = index * 1
+    }
+})
+$("#diyheadBottomTextoneleft li").mouseenter(function (e) {
+    $("#diyheadBottomTextoneright").css({ "width": "780px" }).html("<h1>" + e.target.liindex + "</h1>")
+})
+$("#diyheadBottomTextone").mouseleave(function () {
+    $("#diyheadBottomTextoneright").css({ "width": "0px" }).html("")
+})
+Object.keys($("#diyheadBottomTextthreemain >div:nth-child(1) a")).forEach((index, item) => {
+    if (index * 1 || index * 1 == 0) {
+        $("#diyheadBottomTextthreemain >div:nth-child(1) a")[index].aindex = index * 1
+    }
+})
+$("#diyheadBottomTextthreemain >div:nth-child(1) a").mouseenter(function (e) {
+    $(".showUl").eq(0).removeClass("showUl")
+    $("#diyheadBottomTextthreemain >div:nth-child(1) a").css({ "color": "black", "borderBottom": " solid 1px black" })
+    $("#diyheadBottomTextthreemain >div:nth-child(1) a").eq(e.target.aindex).css({ "color": "rgb(243, 50, 70)", "borderBottom": " solid 1px rgb(243, 50, 70)" })
+    $("#diyheadBottomTextthreemain >div:nth-child(2) ul").eq(e.target.aindex).addClass("showUl")
+    console.log(e.target, $("#diyheadBottomTextthreemain >div:nth-child(2) ul")[e.target.aindex], $(".showUl"))
+})
+$(".floornumberTextThree>div").mouseenter(function (e) {
+    e.currentTarget.children[1].children[1].style = " border:solid 1px orange;color:orange"
+})
+$(".floornumberTextThree>div").mouseleave(function (e) {
+    e.currentTarget.children[1].children[1].style = " border:none;color:black"
+})
+// 回顶和楼层优化
+var time = null
+function floorFun() {
+    time = setInterval(function () {
+        document.documentElement.scrollTop--
+        console.log(document.documentElement.scrollTop)
+    }, 1)
+}
+function floormax(maxnumber) {
+    if (document.documentElement.scrollTop == maxnumber * 1) {
+        clearInterval(time)
+        time = null
+    }
+}
+function fun1(index) {
+    $(".none").eq(index).removeClass("none")
+    $(".floorbutton i").eq(index).addClass("none")
+}
+function fun2(index) {
+    $(".floorbutton div").eq(index).addClass("none")
+    $(".floorbutton i").eq(index).removeClass("none")
+}
+$("#windowtop").click(function () {
+    floorFun()
+})
+Object.keys($(".floornumber")).forEach((index, item) => {
+    if (index * 1 || index * 1 == 0) {
+        $(".floornumber")[index].index = index * 1
+    }
+})
+Object.keys($(".floorbutton")).forEach((index, item) => {
+    if (index * 1 || index * 1 == 0) {
+        $(".floorbutton i")[index].index = index * 1
+        $(".floorbutton div")[index].index = index * 1
+    }
+})
+$(".floorbutton").mouseenter(function (e) {
+    var index = e.target.index
+    fun1(index)
+})
+$(".floorbutton").mouseleave(function (e) {
+    var index = e.target.parentElement.index
+    fun2(index)
+})
 $(window).scroll(function () {
     if (document.documentElement.scrollTop * 1 >= 176) {
-        $("#topinput").css({ "display": "none"})
+        $("#topinput").css({ "display": "none" })
         $("#navinput").css({ "display": "flex" })
-        $("main").css({"top":"50px"})
-        $("footer").css({"top":"50px"})
-        $("#diyhead").css({"top":"50px"})
+        $("main").css({ "top": "50px" })
+        $("footer").css({ "top": "50px" })
+        $("#diyhead").css({ "top": "50px" })
+        $("#floortop").css({"display":"flex"})
     } else {
-        $("#topinput").css({ "display":  "block" })
+        $("#topinput").css({ "display": "block" })
         $("#navinput").css({ "display": "none" })
-        $("#diyhead").css({"top":"0"})
-        $("main").css({"top":"0px"})
-        $("footer").css({"top":"0px"})
+        $("#diyhead").css({ "top": "0" })
+        $("main").css({ "top": "0px" })
+        $("footer").css({ "top": "0px" })
+        $("#floortop").css({"display":"none"})
+    }
+    floormax(0)
+    if (document.documentElement.scrollTop <= $("main")[0].offsetTop+240) {
+        fun2(0)
+        fun2(1)
+        fun2(2)
+        fun2(3)
+        fun2(4)
+        fun2(5)
+        fun2(6)
+        fun2(7)
+    } else if (document.documentElement.scrollTop <= $(".floornumber")[0].offsetTop + $("main")[0].offsetTop+240) {
+        fun1(0)
+        fun2(1)
+        fun2(2)
+        fun2(3)
+        fun2(4)
+        fun2(5)
+        fun2(6)
+        fun2(7)
+    } else if (document.documentElement.scrollTop <= $(".floornumber")[1].offsetTop + $("main")[0].offsetTop+240) {
+        fun2(0)
+        fun1(1)
+        fun2(2)
+        fun2(3)
+        fun2(4)
+        fun2(5)
+        fun2(6)
+        fun2(7)
+    } else if (document.documentElement.scrollTop <= $(".floornumber")[2].offsetTop + $("main")[0].offsetTop+240) {
+        fun2(0)
+        fun2(1)
+        fun1(2)
+        fun2(3)
+        fun2(4)
+        fun2(5)
+        fun2(6)
+        fun2(7)
+    } else if (document.documentElement.scrollTop <= $(".floornumber")[3].offsetTop + $("main")[0].offsetTop+240) {
+        fun2(0)
+        fun2(1)
+        fun2(2)
+        fun1(3)
+        fun2(4)
+        fun2(5)
+        fun2(6)
+        fun2(7)
+    } else if (document.documentElement.scrollTop <= $(".floornumber")[4].offsetTop + $("main")[0].offsetTop+240) {
+        fun2(0)
+        fun2(1)
+        fun2(2)
+        fun2(3)
+        fun1(4)
+        fun2(5)
+        fun2(6)
+        fun2(7)
+    } else if (document.documentElement.scrollTop <= $(".floornumber")[5].offsetTop + $("main")[0].offsetTop+240) {
+        fun2(0)
+        fun2(1)
+        fun2(2)
+        fun2(3)
+        fun2(4)
+        fun1(5)
+        fun2(6)
+        fun2(7)
+    } else if (document.documentElement.scrollTop <= $(".floornumber")[6].offsetTop + $("main")[0].offsetTop+240) {
+        fun2(0)
+        fun2(1)
+        fun2(2)
+        fun2(3)
+        fun2(4)
+        fun2(5)
+        fun1(6)
+        fun2(7)
+    } else if (document.documentElement.scrollTop <= $(".floornumber")[7].offsetTop + $("main")[0].offsetTop+240) {
+        fun2(0)
+        fun2(1)
+        fun2(2)
+        fun2(3)
+        fun2(4)
+        fun2(5)
+        fun2(6)
+        fun1(7)
+    } else {
+        fun2(0)
+        fun2(1)
+        fun2(2)
+        fun2(3)
+        fun2(4)
+        fun2(5)
+        fun2(6)
+        fun1(7)
     }
 })
-Object.keys($("#diyheadBottomTextoneleft li")).forEach((index,item)=>{
-    if(index*1||index*1==0){
-        $("#diyheadBottomTextoneleft li")[index].liindex=index*1
-    }
-})
-$("#diyheadBottomTextoneleft li").mouseenter(function(e){
-    $("#diyheadBottomTextoneright").css({"width":"780px"}).html("<h1>"+e.target.liindex+"</h1>")
-})
-$("#diyheadBottomTextone").mouseleave(function(){
-    $("#diyheadBottomTextoneright").css({"width":"0px"}).html("")
-})
-Object.keys($("#diyheadBottomTextthreemain >div:nth-child(1) a")).forEach((index,item)=>{
-    if(index*1||index*1==0){
-       $("#diyheadBottomTextthreemain >div:nth-child(1) a")[index].aindex=index*1
-    }    
-})
-$("#diyheadBottomTextthreemain >div:nth-child(1) a").mouseenter(function(e){
-    $(".showUl").eq(0).removeClass("showUl")
-    $("#diyheadBottomTextthreemain >div:nth-child(1) a").css({"color": "black","borderBottom":" solid 1px black"})
-    $("#diyheadBottomTextthreemain >div:nth-child(1) a").eq(e.target.aindex).css({"color": "rgb(243, 50, 70)","borderBottom":" solid 1px rgb(243, 50, 70)"})
-    $("#diyheadBottomTextthreemain >div:nth-child(2) ul").eq(e.target.aindex).addClass("showUl")
-    console.log(e.target,$("#diyheadBottomTextthreemain >div:nth-child(2) ul")[e.target.aindex],$(".showUl"))
+$(".floorbutton").click(function(e){
+    document.documentElement.scrollTop=$(".floornumber")[e.target.parentElement.index].offsetTop+ $("main")[0].offsetTop-48
 })
 
 
